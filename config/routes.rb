20f81421cc9560794549ce93e9afd137d317e378
Shingo Matsushima <s.matsushima@sen-do.com>
2017-12-10 04:11:04 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  #TOPページ
   root to: 'toppages#index'
+  
+  #ログインページ
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  #ユーザ登録ページ
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+
 end
